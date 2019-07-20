1abf5e176d37c3443779ea998c91a9acdc53fe4b
Daniel Ahn <93danielahn@gmail.com>
2019-07-20 18:10:18 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
-# Your Code Here
+def map(arr, &prc)
+  arr.map { |ele| prc.call(ele) }
+end
+
+def reduce(arr, starting_point=nil, &prc)
+  i = 0
+  if starting_point.nil?
+    starting_point = arr.first
+    i += 1
+  end
+  while i < arr.length
+    starting_point = prc.call(starting_point, arr[i])
+    i += 1
+  end
+  starting_point
+end
