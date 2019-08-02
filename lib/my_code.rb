49572aeb1f70abb1a15dd6d38063ea6f80703c62
Darrow Palast <dpalast@gmail.com>
2019-08-02 23:21:18 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # Your Code Here
+
+def map(array)
+  
+  new_arr = []
+  array.each do |ele|
+    yield
+  end
+  new_arr
+  
+end
+
+
+map([1, 2, 3, -9]) {|n| n *-1}
+# map([1, 2, 3, -9]) {|n| n}
+map([1, 2, 3, -9]) {|n| n *2}
+map([1, 2, 3, -9]) {|n| n **2}
