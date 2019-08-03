c00de565dbb4da6ff8ac48f370adc12412dbfbf2
Belinda Lei <belinda.lei95@gmail.com>
2019-08-03 23:22:43 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+
+def map(array)
+  new_array = []
+  array.each do |item|
+    new_array << yield(item)
+  end
+  new_array
+end
+
+def reduce(array, start = 0)
+  if start != 0
+    memo = start
+  elsif array.size
+    memo = array[0]
+    array = array.drop(1)
+  else
+    return 'empty array'
+  end
+
+  array.each do |n|
+    memo = yield(memo, n)
+  end 
+  memo
+end
+
+
+
+
